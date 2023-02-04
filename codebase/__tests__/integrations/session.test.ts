93ed9fec5adb5ad840aa7444e9cf20ef1e652e48
Gustavo Martins <gh.martins9@gmail.com>
2023-02-04 14:58:17 -0300

    Configurando o JEST

---
+++ codebase/__tests__/integrations/session.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test } from '@jest/globals';
+import { CreateUserService } from '../../src/services/CreateUserService';
+
+describe('Autenticação', () => {
+    test('Cadastro de usuário valido', async () => {
+        const newUser = {
+            name: 'Gustavo',
+            email: 'gustavo@email.com',
+            senha: 'teste123',
+            logradouro: null,
+            numero: null,
+            complemento: null,
+            bairro: null,
+            cidade: null,
+            estado: null,
+            telefone: null,
+            CEP: null
+        }
+
+        const createUserService = new CreateUserService();
+
+        expect(await createUserService.execute(newUser)).toBeUndefined()
+    })
+})
