c815f2bb87ca5a0d3bf2cab2760c1c0488517364
Gustavo Martins <gh.martins9@gmail.com>
2023-02-08 20:27:27 -0300

    Update session.test.ts

--- codebase/__tests__/integrations/session.test.ts
+++ codebase/__tests__/integrations/session.test.ts
@@ -21,4 +21,24 @@ describe('Autenticação', () => {
 
         expect(await createUserService.execute(newUser)).toBeUndefined()
     })
+
+    test('Cadastro de usuário valido', async () => {
+        const newUser = {
+            name: 'Gustavo',
+            email: 'gustavo@email.com',
+            senha: 'teste123',
+            logradouro: null,
+            numero: null,
+            complemento: null,
+            bairro: null,
+            cidade: null,
+            estado: null,
+            telefone: null,
+            CEP: null
+        }
+
+        const createUserService = new CreateUserService();
+
+        expect(await createUserService.execute(newUser)).toBeUndefined()
+    })
 })
